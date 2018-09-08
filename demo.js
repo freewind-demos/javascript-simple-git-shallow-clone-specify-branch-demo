79889fb43a39ac693b63f5d72e3b245db739a47b
freewind <nowindlee@gmail.com>
2018-09-08 22:56:47 +0800

    improve code

--- demo.js
+++ demo.js
@@ -2,5 +2,6 @@ const workingDirPath = './local-repo'
 const simpleGit = require('simple-git')(workingDirPath)
 
 simpleGit.clone('https://github.com/freewind-demos/git-shallow-clone-checkout-remote-branch-demo.git', 'demo-project', ['--depth=1', '--branch=admin'])
-
-console.log('shallow cloned to: local-repo/demo-project')
+    .exec(function () {
+        console.log('shallow cloned to: local-repo/demo-project')
+    })
