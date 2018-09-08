aa33f0e5228121ae86246c40b889344834666c09
freewind <nowindlee@gmail.com>
2018-09-08 22:22:40 +0800

    shallow clone demo

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-JavaScript "simple-git" Clone Demo
-==================================
+JavaScript "simple-git" Shallow Clone Demo
+==========================================
 
-使用"simple-git"将指定的git仓库clone到本地
+"Shallow Clone"是指使用`--depth=1`参数，只取最新的一个commit.
 
 ```
 npm install
@@ -9,3 +9,11 @@ node run demo
 ```
 
 然后就会在`local-repo`目录下看到新clone下来的`demo-project`。
+
+检查一下是不是shallow clone:
+
+```
+$ cd local-repo/demo-project
+$ git rev-list --count master
+1
+```
--- demo.js
+++ demo.js
@@ -1,6 +1,6 @@
 const workingDirPath = './local-repo'
 const simpleGit = require('simple-git')(workingDirPath)
 
-simpleGit.clone('https://github.com/freewind-demos/javascript-frontend-hello-world-demo.git', 'demo-project')
+simpleGit.clone('https://github.com/freewind-demos/git-shallow-clone-checkout-remote-branch-demo.git', 'demo-project', '--depth=1')
 
-console.log('cloned to: local-repo/demo-project')
+console.log('shallow cloned to: local-repo/demo-project')
